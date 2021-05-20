cb3a4dc2cb2036c624ed9ae0b0198d347cacce2a
10k-resistor <robotx2016@gmx.de>
2021-05-20 07:59:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// JavaScript source code
+var qrText = "no data"
+var qrc;
+
+// set timer
+var myVar = setInterval(generateClock, 1000);
+
+function generateQRCode(){
+    qrc.clear(); // clear the code.
+    qrc.makeCode(qrText); // make another code.
+}
+
+function generateClock(){
+    var d = new Date();
+    qrText = d.toLocaleTimeString('en-US');
+    //console.log(qrText);
+    document.getElementById("actualTime").innerHTML = qrText;
+
+    generateQRCode();
+}
+
+function start(){
+    console.log("start");
+    qrc = new QRCode(document.getElementById("qrcode"), "no data");
+    generateClock();
+
+}
